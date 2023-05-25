470a03f46f24bfc586013479566f21750e74682d
Alan Andrade <alan@fasters.com.br>
2023-05-25 19:18:05 -0300

    fix: fix checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,31 +2,18 @@
 
 import { Check } from 'phosphor-react';
 import { Root, Indicator } from '@radix-ui/react-checkbox';
-import { ComponentProps, useState } from 'react';
+import { ComponentProps } from 'react';
 
 export type CheckboxProps = ComponentProps<typeof Root>;
 
 export function Checkbox({ onClick, ...props }: CheckboxProps) {
-  const [checked, setChecked] = useState(false);
-
-  const handleOnClick = (e: any) => {
-    setChecked((prevState) => !prevState);
-
-    if (onClick) {
-      onClick(e);
-    }
-  };
-
   return (
     <Root
-      className={`${
-        checked ? 'checked border-green-300 bg-green-300' : 'unchecked'
-      } group box-border flex h-6 w-6 cursor-pointer items-center justify-center overflow-hidden rounded border-2 border-gray-900 bg-gray-900 leading-[0] focus:border-green-300`}
-      onClick={handleOnClick}
+      className="box-border flex h-6 w-6 cursor-pointer items-center justify-center overflow-hidden rounded border-2 border-gray-900 bg-gray-900 leading-[0] focus:border-green-300 data-[state=checked]:border-green-300 data-[state=checked]:bg-green-300"
       {...props}
     >
       <Indicator
-        className={`h-4 w-4 text-white group-[.checked]:animate-slideIn group-[.unchecked]:animate-slideOut`}
+        className={`h-4 w-4 text-white data-[state=checked]:animate-slideIn data-[state=unchecked]:animate-slideOut`}
         asChild
       >
         <Check weight="bold" />
