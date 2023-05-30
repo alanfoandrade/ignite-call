ce1c521362aea9e925c742f394a9371af06174db
Alan Andrade <alan@fasters.com.br>
2023-05-29 22:00:50 -0300

    feat: add react hook form

--- src/app/(home)/components/ClaimUsernameForm.tsx
+++ src/app/(home)/components/ClaimUsernameForm.tsx
@@ -1,8 +1,25 @@
+'use client';
+
 import { Button } from '@/components/Button';
 import { TextInput } from '@/components/TextInput';
 import { ArrowRight } from 'lucide-react';
 import { FormHTMLAttributes } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { twMerge } from 'tailwind-merge';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Mínimo 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'Apenas letras ou hifens',
+    })
+    .transform((value) => value.toLowerCase()),
+});
+
+type ClaimusernameFormData = z.infer<typeof claimUsernameFormSchema>;
 
 interface ClaimUsernameFormProps extends FormHTMLAttributes<HTMLFormElement> {}
 
@@ -10,20 +27,50 @@ export function ClaimUsernameForm({
   className,
   ...props
 }: ClaimUsernameFormProps) {
-  return (
-    <form
-      {...props}
-      className={twMerge(
-        'border-1 mt-4 grid grid-cols-[1fr_auto] gap-2 rounded-lg border-gray-600 bg-gray-800 p-4 max-lg:grid-cols-1',
-        className,
-      )}
-    >
-      <TextInput prefix="hiapp.com/" variant="sm" placeholder="seu-usuario" />
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimusernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  });
 
-      <Button size="sm" type="submit">
-        Reservar usuário
-        <ArrowRight className="h-4 w-4" />
-      </Button>
-    </form>
+  const handleClaimUsername: SubmitHandler<ClaimusernameFormData> = async (
+    data,
+  ) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <form
+        {...props}
+        className={twMerge(
+          'border-1 mt-4 grid grid-cols-[1fr_auto] gap-2 rounded-lg border-gray-600 bg-gray-800 p-4 max-lg:grid-cols-1',
+          className,
+        )}
+        onSubmit={handleSubmit(handleClaimUsername)}
+      >
+        <input className="hidden" />
+
+        <TextInput
+          prefix="hiapp.com/"
+          variant="sm"
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
+
+        <Button size="sm" type="submit">
+          Reservar usuário
+          <ArrowRight className="h-4 w-4" />
+        </Button>
+      </form>
+
+      <div className="absolute mt-1 px-1">
+        <p className="text-sm text-gray-400">
+          {errors.username?.message || 'Digite um nome de usuário'}
+        </p>
+      </div>
+    </>
   );
 }
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -3,15 +3,17 @@ import {
   InputHTMLAttributes,
   forwardRef,
 } from 'react';
+import { FieldError } from 'react-hook-form';
 import { twMerge } from 'tailwind-merge';
 
 interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   prefix?: string;
   variant?: 'sm' | 'md';
+  error?: FieldError;
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, TextInputProps> = (
-  { prefix, variant = 'md', className, ...props },
+  { prefix, variant = 'md', className, error = null, ...props },
   ref,
 ) => {
   let variantStyles = '';
@@ -29,20 +31,30 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, TextInputProps> = (
   }
 
   return (
-    <div
-      className={twMerge(
-        `box-border flex items-center rounded-md border-2 border-gray-900 bg-gray-900 group-focus:border-green-300 group-disabled:cursor-not-allowed group-disabled:opacity-50`,
-        variantStyles,
-        className,
-      )}
-    >
-      {!!prefix && <span className="text-sm text-gray-400">{prefix}</span>}
+    <div>
+      <div
+        className={twMerge(
+          `box-border flex items-center rounded-md border-2 border-gray-900 bg-gray-900 group-focus:border-green-300 group-disabled:cursor-not-allowed group-disabled:opacity-50`,
+          variantStyles,
+          className,
+        )}
+      >
+        {!!prefix && <span className="text-sm text-gray-400">{prefix}</span>}
 
-      <input
-        {...props}
-        className="group w-full border-0 bg-transparent text-sm placeholder:text-gray-400 focus:outline-none disabled:cursor-not-allowed"
-        ref={ref}
-      />
+        <input
+          {...props}
+          className="group w-full border-0 bg-transparent text-sm placeholder:text-gray-400 focus:outline-none disabled:cursor-not-allowed"
+          ref={ref}
+        />
+      </div>
+
+      {!!error && (
+        <div className="absolute mt-1 px-1">
+          <p className="text-sm text-gray-400">
+            {error.message || 'Digite um nome de usuário'}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
